ace20522e021f8ea75b5959e69948bdf98fa9de0
javascriptninjazxc <henexy@mail.ru>
2025-05-05 20:14:24 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,9 @@ import { CustomValidationPipe } from './pipes/custom-validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const CLIENT_URL = config.getOrThrow<string>('CLIENT_URL');
   app.useGlobalPipes(new CustomValidationPipe());
 
-  app.enableCors({
-    credentials: true,
-    origin: CLIENT_URL,
-  });
+  app.enableCors();
 
   await app.listen(3000);
 }
